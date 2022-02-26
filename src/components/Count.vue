6644e908145c02953b74d43795de23f368720a84
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-02-26 20:03:15 +0800

    feat:init project

---
+++ src/components/Count.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>{{count}}</text>
+		<div @click="add" >add</div>
+	</view>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+const count: Ref<number> = ref<number>(1);
+
+const add = () => {
+	count.value++;
+}
+</script>
+
+<style>
+
+</style>
